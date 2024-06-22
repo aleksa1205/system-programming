1365b996eb732e83913af83e8dcff59de4d13cc6
Jovan Cvetkovic <cvetkovicjovanwork@gmail.com>
2024-06-22 19:15:53 +0200

    Dodata jos 2 observera da slusaju na zadnji observable

--- News_HTTPServer_Reactive/News_HTTPServer_Reactive/NYTimes_HTTPServer_Reactive/src/Program.cs
+++ News_HTTPServer_Reactive/News_HTTPServer_Reactive/NYTimes_HTTPServer_Reactive/src/Program.cs
@@ -1,4 +1,7 @@
-﻿using NYTimes_HTTPServer_Reactive.Observers;
+﻿using System.Net;
+using System.Reactive;
+using NYTimes_HTTPServer_Reactive.AiModel;
+using NYTimes_HTTPServer_Reactive.Observers;
 using NYTimes_HTTPServer_Reactive.ReactiveLayers;
 
 var model = new ModelTraining();
@@ -10,8 +13,18 @@ var serverSubscription = model.Subscribe(server);
 var newsApiSubscription = server.Subscribe(newsApiCall);
 var analysisSubscription = newsApiCall.Subscribe(analysis);
 
-var observer = new HttpResponseObserver();
-var observerSubscription = analysis.Subscribe(observer);
+var httpResponseObserver = new HttpResponseObserver();
+
+var consoleObserver1 = Observer.Create<(HttpListenerContext, List<SentimentPrediction>)>(
+    onNext: (value) => Console.WriteLine($"Observer 1: {value.Item2[0].Prediction}")
+);
+var consoleObserver2 = Observer.Create<(HttpListenerContext, List<SentimentPrediction>)>(
+    onNext: (value) => Console.WriteLine($"Observer 2: {value.Item2[2].Prediction}")
+);
+
+var observerSubscription = analysis.Subscribe(httpResponseObserver);
+var observer2Subscription = analysis.Subscribe(consoleObserver1);
+var observer3Subscription = analysis.Subscribe(consoleObserver2);
 
 model.StartTraining();
 
@@ -22,5 +35,5 @@ serverSubscription.Dispose();
 newsApiSubscription.Dispose();
 analysisSubscription.Dispose();
 observerSubscription.Dispose();
-
-return;
+observer2Subscription.Dispose();
+observer3Subscription.Dispose();
